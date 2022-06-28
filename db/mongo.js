eb85044dc252e55a8a6cb7b3bc3320317506da33
RizFurr <101236698+RizFurr@users.noreply.github.com>
2022-06-28 19:58:02 +0800

    Update mongo.js

--- db/mongo.js
+++ db/mongo.js
@@ -1,7 +1,7 @@
 const monk = require('monk')
 
 // Connection URL
-var url = "";
+var url = "mongodb+srv://RizFurr:uwu@cluster0.zm5ah.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";  //ganti dengan url mongoDB kamu
 try {
     if (url == '') throw console.log('Cek konfigurasi database, var url belum diisi');
 } catch (e) {
